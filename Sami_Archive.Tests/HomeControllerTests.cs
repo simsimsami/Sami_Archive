e58339c69c344982e241c133e3f1f4176a4ca36a
simsimsami <samihamdan@hotmail.co.uk>
2026-02-13 21:11:25 +0000

    Adding pagination, infrastructure folder with the pagelinktaghelper, as well as a test suit with 4 tests, 3 pass but 1 fail for the moment

---
+++ Sami_Archive.Tests/HomeControllerTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using Xunit.Sdk;
+using Sami_Archive.Models;
+using Sami_Archive.Controllers;
+using Sami_Archive.Models.ViewModels;
+
+namespace Sami_Archive.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arrange the mock data
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns((new Book[]
+            {
+                new Book { BookID = 1, Title = "B1", Description = "D1", Genre = "G1" },
+                new Book { BookID = 2, Title = "B2", Description = "D2", Genre = "G2" },
+                new Book { BookID = 3, Title = "B3", Description = "D3", Genre = "G3" },
+                new Book { BookID = 4, Title = "B4", Description = "D4", Genre = "G4" },
+                new Book { BookID = 5, Title = "B5", Description = "D5", Genre = "G5" },
+            }).AsQueryable<Book>());
+
+            // Arrange the controller and page size
+            HomeController controller = new HomeController(mock.Object) { PageSize = 2};
+
+            // Act - declare a view model
+            BooksListViewModels result = controller.Index(null, 1)?.ViewData.Model as BooksListViewModels ?? new();
+
+            // Assert the pagination view model
+            
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.NotNull(pageInfo);
+            Assert.Equal(1, pageInfo.CurrentPage);
+            Assert.NotEqual(3, pageInfo.ItemsPerPage);
+            Assert.Equal(2, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(3, pageInfo.TotalPages);
+
+        }
+        [Fact]
+        public void Can_Paginate()
+        {
+            // Arrange - Declaring the object mock - giving it mock data.
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns((new Book[]
+            {
+                new Book { BookID = 1, Title = "B1", Description = "D1", Genre = "G1" },
+                new Book { BookID = 2, Title = "B2", Description = "D2", Genre = "G2" },
+                new Book { BookID = 3, Title = "B3", Description = "D3", Genre = "G3" },
+                new Book { BookID = 4, Title = "B4", Description = "D4", Genre = "G4" },
+                new Book { BookID = 5, Title = "B5", Description = "D5", Genre = "G5" },
+            }).AsQueryable<Book>());
+
+            HomeController controller = new HomeController(mock.Object) { PageSize = 3 };
+
+            // Act - no filters, looking at the second page
+            BooksListViewModels result = controller.Index(null, 2, null)?.ViewData.Model as BooksListViewModels ?? new();
+
+            Book[] bookArray = result.Books.ToArray();
+            Assert.True(bookArray.Length == 2);
+            Assert.Equal("B4", bookArray[0].Title);
+            Assert.Equal("B5", bookArray[1].Title);
+            Assert.NotEqual("B1", bookArray[1].Title);
+        }
+
+        [Fact]
+        public void Can_Access_Repository()
+        {
+            // Arrange - mock data
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns((new Book[]
+            {
+                new Book { BookID = 1, Title = "B1", Description = "D1", Genre = "G1" },
+                new Book { BookID = 2, Title = "B2", Description = "D2", Genre = "G2" },
+                new Book { BookID = 3, Title = "B3", Description = "D3", Genre = "G3" },
+                new Book { BookID = 4, Title = "B4", Description = "D4", Genre = "G4" },
+                new Book { BookID = 5, Title = "B5", Description = "D5", Genre = "G5" },
+            }).AsQueryable<Book>());
+
+            HomeController controller = new HomeController(mock.Object) { PageSize = 3 };
+
+            // Act - getting access to the repo
+            BooksListViewModels result = controller.Index(null, 2, null)?.ViewData.Model as BooksListViewModels ?? new();
+
+            // Assert - checking if the controller can access the repository
+            Book[] bookArray = result.Books.ToArray();
+            Assert.True(bookArray.Length == 2);
+            Assert.NotNull(bookArray);
+
+        }
+    }
+}
--- Sami_Archive/Models/SeedData.cs
+++ Sami_Archive/Models/SeedData.cs
@@ -19,8 +19,101 @@ namespace Sami_Archive.Models
                 context.Books.AddRange(
                     new Book
                     {
-                        Title = "Necromancer Supreme Vol 1", Description = "Introduction to the first necromancer", Genre = "Fantasy"
-                    });
+                        Title = "Necromancer Supreme Vol 1",
+                        Description = "Introduction to the first necromancer",
+                        Genre = "Fantasy",
+                    },
+                    new Book
+                    {
+                        Title = "Farmers Love",
+                        Description = "Farming and the great outdoors",
+                        Genre = "Drama"
+                    },
+                    new Book
+                    {
+                        Title = "John Q",
+                        Description = "A colorful variety of crunchy veggie chips for snacking.",
+                        Genre = "Crime"
+                    },
+                    new Book
+                    {
+                        Title = "Vow, The",
+                        Description = "Vegetable spiralizer for healthy meals.",
+                        Genre = "Drama"
+                    },
+                    new Book
+                    {
+                        Title = "25 Watts",
+                        Description = "GPS pet collar that helps locate your pet via smartphone app.",
+                        Genre = "Comedy"
+                    },
+                    new Book
+                    {
+                        Title = "Mustalaishurmaaja",
+                        Description = "Cute dispenser for bathrooms or kitchens featuring paw prints.",
+                        Genre = "Drama"
+                    },
+                    new Book
+                    {
+                        Title = "16 Years of Alcohol",
+                        Description = "Creamy macaroni and cheese baked to perfection.",
+                        Genre = "Drama"
+                    },
+                    new Book
+                    {
+                        Title = "Por un puñado de besos",
+                        Description = "Elegant cover that protects your notebook with style.",
+                        Genre = "Drama"
+                    },
+                    new Book
+                    {
+                        Title = "Gertie the Dinosaur",
+                        Description = "Golden-brown fritters made with sweet corn.",
+                        Genre = "Animation"
+                    },
+                    new Book
+                    {
+                        Title = "Goats",
+                        Description = "Delicious ravioli filled with creamy ricotta and fresh spinach.",
+                        Genre = "Comedy"
+                    },
+                    new Book
+                    {
+                        Title = "Page Turner, The (Tourneuse de pages, La)",
+                        Description = "Color-changing 3D night light for kids' rooms.",
+                        Genre = "Drama"
+                    },
+                    new Book
+                    {
+                        Title = "God Bless America",
+                        Description = "Complete kit to make your own flavored lip balms at home.",
+                        Genre = "Comedy"
+                    },
+                    new Book
+                    {
+                        Title = "Promised Land",
+                        Description = "Countertop dishwasher for small kitchens.",
+                        Genre = "Drama"
+                    },
+                    new Book
+                    {
+                        Title = "Abbott and Costello Meet Dr. Jekyll and Mr. Hyde",
+                        Description = "Complete kit for starting your own organic garden.",
+                        Genre = "Comedy"
+                    },
+                    new Book
+                    {
+                        Title = "Love Actually",
+                        Description = "Seat belt attachment to keep dogs safe during car rides.",
+                        Genre = "Comedy"
+                    },
+                    new Book
+                    {
+                        Title = "Critical Care",
+                        Description = "Sweet corn roasted to perfection for a delightful side.",
+                        Genre = "Comedy"
+                    }
+                    );
                 context.SaveChanges();
             }
         }
