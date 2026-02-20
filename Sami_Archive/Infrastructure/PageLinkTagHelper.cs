16565bc8a5ae14d7027eb4c3c21973b333597c96
simsimsami <samihamdan@hotmail.co.uk>
2026-02-20 21:23:15 +0000

    implemented the api for books, now planning to add the frontend - so that I can work towards adding a form to add books

--- Sami_Archive/Infrastructure/PageLinkTagHelper.cs
+++ Sami_Archive/Infrastructure/PageLinkTagHelper.cs
@@ -19,6 +19,11 @@ namespace Sami_Archive.Infrastructure
         public PagingInfo? PageModel { get; set; }
         public string? PageAction { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+        public string PageClassNormal {  get; set; } = String.Empty;
+        public string PageClassSelected {  get; set; } = String.Empty;
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
@@ -29,6 +34,11 @@ namespace Sami_Archive.Infrastructure
                 {
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { bookPage = i });
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
                     tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
                 }
